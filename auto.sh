a481a827cf94f866a0ece727ad9dff491d09684e
Baohua Yang <yangbaohua@gmail.com>
2012-07-18 16:07:46 +0800

    merge and modify auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,34 @@
 #This is a temporal script to test quick operations, e.g., generating docs.
 
-#Quick install openvswitch
-./boot.sh && ./configure && make && sudo make install;
+#Configure openvswitch
+./boot.sh && ./configure  --with-linux=/lib/modules/`uname -r`/build;
 
+#Compile and install openvswitch
+make && sudo make install;
+
+#insmod datapath/linux/openvswitch.ko
+modprobe datapath/linux/openvswitch.ko
+
+#configure the ovs-db
+mkdir -p /usr/local/etc/openvswitch
+ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+
+#start the ovs-db server
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+             --remote=db:Open_vSwitch,manager_options \
+             --private-key=db:SSL,private_key \
+             --certificate=db:SSL,certificate \
+             --bootstrap-ca-cert=db:SSL,ca_cert \
+             --pidfile --detach
+
+#Initialize the ovs-db
+ovs-vsctl --no-wait init
+
+#start the main ovs daemon
+ovs-vswitchd --pidfile --detach
+
+#create a new vs
+ovs-vsctl add-br br0
+
+#stop the ovs daemon
+kill `cd /usr/local/var/run/openvswitch && cat ovsdb-server.pid ovs-vswitchd.pid`
