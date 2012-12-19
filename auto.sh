c84feb6ca670c28cbb3251376caa865a8e0b4e57
baohua <yangbaohua@gmail.com>
2012-12-19 00:01:15 -0800

    change auto.sh

--- auto.sh
+++ auto.sh
@@ -27,7 +27,7 @@ for ((i=1; i<=${NUM}; i++)); do
     sudo arp -s 10.0.0.`expr $i \* 2 - 1` 08:00:27:85:ca:de
 done
 
-ping -c 1 192.168.58.1
+ping -c 1 192.168.58.1 >/dev/null&
 
 #thu
 #sudo arp -s 10.0.0.2 08:00:27:ab:b6:a5;
